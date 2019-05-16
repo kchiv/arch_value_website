6e9447663be0124745492dc0264a2151f4c78081
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-16 12:43:36 -0700

    specified template directory

--- arch_value_solutions/settings.py
+++ arch_value_solutions/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'arch_value_solutions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
