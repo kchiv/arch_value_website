1b717af30e41f650f598da2078ccf705087ddc27
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-17 13:08:24 -0700

    setup partner app

--- arch_value_solutions/urls.py
+++ arch_value_solutions/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('events/', include('events.urls'))
+    path('events/', include('events.urls')),
+    path('partners/', include('partners.urls')),
 ]
