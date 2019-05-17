542cd10568ed11553294a389b54bf624b2de16c8
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-17 12:40:09 -0700

    basic event model setup

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Event(models.Model):
+	event_name = models.CharField(max_length=200)
+	event_start = models.DateField(default=datetime.date.today)
+	event_end = models.DateField(default=datetime.date.today)
+	event_address = models.CharField(max_length=500)
+
+	def __str__(self):
+		return self.event_name
