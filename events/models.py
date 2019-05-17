cc0a47e2454985832e6c47273926972930d1346a
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-17 12:50:19 -0700

    migrated event model

--- events/models.py
+++ events/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from datetime import datetime
+from datetime import date
 
 # Create your models here.
 
 class Event(models.Model):
 	event_name = models.CharField(max_length=200)
-	event_start = models.DateField(default=datetime.date.today)
-	event_end = models.DateField(default=datetime.date.today)
+	event_start = models.DateField(default=date.today)
+	event_end = models.DateField(default=date.today)
 	event_address = models.CharField(max_length=500)
 
 	def __str__(self):
