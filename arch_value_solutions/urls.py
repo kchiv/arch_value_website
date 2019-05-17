74278382ac0e846559fa701c811f12fdf44591c4
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-17 15:21:21 -0700

    routing for filebrowser

--- arch_value_solutions/urls.py
+++ arch_value_solutions/urls.py
@@ -13,12 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, re_path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from filebrowser.sites import site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^admin/filebrowser/', site.urls),
     re_path(r'^tinymce/', include('tinymce.urls')),
     path('events/', include('events.urls')),
     path('partners/', include('partners.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
