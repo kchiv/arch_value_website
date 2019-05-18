af800bc88345bdfe41e2830fd825c99760f49fa0
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-17 18:31:48 -0700

    meta class added to category model

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ from filebrowser.fields import FileBrowseField
 class Category(models.Model):
 	category_name = models.CharField(max_length=200)
 	is_published = models.BooleanField(default=True)
-	category_slug = models.SlugField(unique=True)
+	category_slug = models.SlugField(blank=True, unique=True)
 	category_image = FileBrowseField('Category image', max_length=1000, extensions=['.jpg', 
 														'.jpeg', 
 														'.gif', 
@@ -17,13 +17,15 @@ class Category(models.Model):
 														'.tiff'], blank=True)
 	category_content = HTMLField('Category content', blank=True)
 
-
+	class Meta:
+		verbose_name_plural = 'categories'
+	
 	def __str__(self):
 		return self.category_name
 
 class Tag(models.Model):
 	tag_name = models.CharField(max_length=300)
-	tag_slug = models.SlugField(unique=True)
+	tag_slug = models.SlugField(blank=True, unique=True)
 
 	def __str__(self):
 		return self.tag_name
@@ -32,8 +34,8 @@ class Post(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, models.SET_NULL, blank=True, null=True)
 	is_published = models.BooleanField(default=True)
 	title = models.CharField(max_length=700)
-	post_slug = models.SlugField(unique=True)
-	publication_date = models.DateTimeField(auto_now_add=True)
+	post_slug = models.SlugField(blank=True, unique=True)
+	publication_date = models.DateTimeField(blank=True, auto_now_add=True)
 	featured_image = FileBrowseField('Featured image', max_length=1000, extensions=['.jpg', 
 														'.jpeg', 
 														'.gif', 
