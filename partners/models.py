a9eaf16516f73c95d2112dfd5b4a8d4f8471953d
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-18 13:27:37 -0700

    added partner url and tags

--- partners/models.py
+++ partners/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from posts.models import Tag
 from tinymce import HTMLField
 from filebrowser.fields import FileBrowseField
 
@@ -12,7 +13,9 @@ class Partner(models.Model):
 														'.png', 
 														'.tif', 
 														'.tiff'], blank=True)
+	partner_url = models.URLField(blank=True, help_text='Enter the URL for partner website.')
 	partner_description = HTMLField('Partner description', blank=True)
+	partner_tag = models.ForeignKey(Tag, models.SET_NULL, blank=True, null=True, help_text='Select tag for the partner if it exists. This will populate blog posts with that tag.')
 
 	def __str__(self):
 		return self.partner_name
