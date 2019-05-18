6e9c9048ed3a2a9118cd7c27167a63fafb34d8e4
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-17 17:58:21 -0700

    many to many field

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from partners.models import Partner
 from datetime import date
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Event(models.Model):
 	event_start = models.DateField(default=date.today)
 	event_end = models.DateField(default=date.today)
 	event_address = models.CharField(max_length=500)
+	event_partners = models.ManyToManyField(Partner)
 
 	def __str__(self):
 		return self.event_name
