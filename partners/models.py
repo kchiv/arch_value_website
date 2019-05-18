675d477ec51150c1e95dc87b1b5aa3e4ec62eb09
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-17 18:00:12 -0700

    expanded models

--- partners/models.py
+++ partners/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from tinymce import HTMLField
+from filebrowser.fields import FileBrowseField
 
 # Create your models here.
 
 class Partner(models.Model):
 	partner_name = models.CharField(max_length=200)
+	partner_logo = FileBrowseField('Partner logo', max_length=1000, extensions=['.jpg', 
+														'.jpeg', 
+														'.gif', 
+														'.png', 
+														'.tif', 
+														'.tiff'], blank=True)
+	partner_description = HTMLField('Partner description', blank=True)
+
+	def __str__(self):
+		return self.partner_name
--- posts/models.py
+++ posts/models.py
@@ -4,9 +4,44 @@ from tinymce import HTMLField
 # Create your models here.
 
 class Post(models.Model):
-	title = models.CharField(max_length=1000)
-	content = HTMLField('Content')
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=SET_NULL)
+	is_published = models.BooleanField(default=True)
+	title = models.CharField(max_length=700)
+	post_slug = models.SlugField(unique=True)
+	publication_date = models.DateTimeField(auto_now_add=True)
+	featured_image = FileBrowseField('Featured image', max_length=1000, extensions=['.jpg', 
+														'.jpeg', 
+														'.gif', 
+														'.png', 
+														'.tif', 
+														'.tiff'], blank=True)
+	post_content = HTMLField('Content', blank=True)
+	post_category = models.ManyToManyField(Category)
+	post_tag = models.ManyToManyField(Tag)
 
 
 	def __str__(self):
 		return self.title
+
+class Category(models.Model):
+	category_name = models.CharField(max_length=200)
+	is_published = models.BooleanField(default=True)
+	category_slug = models.SlugField(unique=True)
+	category_image = FileBrowseField('Category image', max_length=1000, extensions=['.jpg', 
+														'.jpeg', 
+														'.gif', 
+														'.png', 
+														'.tif', 
+														'.tiff'], blank=True)
+	category_content = HTMLField('Category content', blank=True)
+
+
+	def __str__(self):
+		return self.category_name
+
+class Tag(models.Model):
+	tag_name = models.CharField(max_length=300)
+	tag_slug = models.SlugField(unique=True)
+
+	def __str__(self):
+		return self.tag_name
