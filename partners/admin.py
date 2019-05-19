1d6e5d7c0fb2770805d817c46947944c32d85983
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-18 17:34:38 -0700

    admin customization

--- partners/admin.py
+++ partners/admin.py
@@ -4,4 +4,10 @@ from .models import Partner
 
 # Register your models here.
 
-admin.site.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+	list_display = ('partner_name',)
+	list_display_links = ('partner_name',)
+	search_fields = ('partner_name', 'partner_description')
+	list_per_page = 25
+
+admin.site.register(Partner, PartnerAdmin)
