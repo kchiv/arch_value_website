3fc7468ce7f98856d3c64e677e3ae899967a3e68
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-19 13:33:32 -0700

    added country and description fields

--- events/models.py
+++ events/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from partners.models import Partner
 from datetime import date
 from localflavor.us.models import USStateField
+from django_countries.fields import CountryField
 
 # Create your models here.
 
@@ -10,10 +11,13 @@ class Event(models.Model):
 	is_published = models.BooleanField(default=True, help_text='Check the box to publish event.')
 	event_start = models.DateField(default=date.today)
 	event_end = models.DateField(default=date.today)
+	event_description = models.TextField(blank=True, help_text='Brief description of event.')
+	event_country = CountryField(blank=True, null=True)
 	event_street = models.CharField(max_length=200, blank=True)
 	event_city = models.CharField(max_length=100, blank=True)
-	event_state = USStateField(blank=True)
+	event_state = USStateField(null=True, blank=True, verbose_name='Event state (US only)')
 	event_partners = models.ManyToManyField(Partner, blank=True)
+	# create fields for international events
 
 	def __str__(self):
 		return self.event_name
