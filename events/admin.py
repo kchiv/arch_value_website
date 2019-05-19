fbf1d31a0dc854b52099baa37ea0d25765f2efa7
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-18 17:34:23 -0700

    admin customization

--- events/admin.py
+++ events/admin.py
@@ -4,8 +4,13 @@ from .models import Event
 # Register your models here.
 
 class EventAdmin(admin.ModelAdmin):
-	list_display = ('event_name', 'event_start', 'event_end', 'event_address')
+	list_display = ('event_name', 'event_start', 'event_end', 'event_address', 'get_partners')
 	list_display_links = ('event_name',)
 	search_fields = ('event_name', 'event_address', 'event_partners')
+	list_per_page = 25
+
+	def get_partners(self, obj):
+		return ', '.join([p.partner_name for p in obj.event_partners.all()])
+	get_partners.short_description = 'Partners'
 
 admin.site.register(Event, EventAdmin)
