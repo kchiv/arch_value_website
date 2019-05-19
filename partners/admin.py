007eeac28e8edd81b2340bc6cc3f862d594a5829
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-19 07:55:48 -0700

    show image in admin

--- partners/admin.py
+++ partners/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from .models import Partner
 
 # Register your models here.
 
 class PartnerAdmin(admin.ModelAdmin):
-	list_display = ('partner_name',)
-	list_display_links = ('partner_name',)
+	list_display = ('partner_name', 'get_image')
+	list_display_links = ('partner_name', 'get_image')
 	search_fields = ('partner_name', 'partner_description')
 	list_per_page = 25
+
+	def get_image(self, obj):
+		# show image in object grid
+		if obj.partner_logo:
+			# must check if object exists otherwise throws an error
+			return mark_safe('<img src="{thumb}" width="100" />'.format(thumb=obj.partner_logo.url,))
+		else:
+			return 'No image available'
+	get_image.allow_tags = True
+	get_image.short_description = 'Partner logo'
 
 admin.site.register(Partner, PartnerAdmin)
--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post, Category, Tag
 
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ('meta_title', 'author', 'publication_date', 'is_published', 'get_categories')
+	list_display = ('meta_title', 'author', 'publication_date', 'is_published', 'get_categories', 'get_image')
 	list_display_links = ('meta_title',)
 	search_fields = ('meta_title', 'partner_description')
 	list_editable = ('is_published',)
@@ -13,6 +14,16 @@ class PostAdmin(admin.ModelAdmin):
 	def get_categories(self, obj):
 		return ', '.join([c.category_name for c in obj.post_category.all()])
 	get_categories.short_description = 'Categories'
+
+	def get_image(self, obj):
+		# show image in object grid
+		if obj.featured_image:
+			# must check if object exists otherwise throws an error
+			return mark_safe('<img src="{thumb}" width="100" />'.format(thumb=obj.featured_image.url,))
+		else:
+			return 'No image available'
+	get_image.allow_tags = True
+	get_image.short_description = 'Featured Image'
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
