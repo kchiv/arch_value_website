4007f95a10b7a18209785a20e880f9bd47353763
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-18 17:35:16 -0700

    customized post admin

--- posts/admin.py
+++ posts/admin.py
@@ -3,6 +3,17 @@ from .models import Post, Category, Tag
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('meta_title', 'author', 'publication_date', 'is_published', 'get_categories')
+	list_display_links = ('meta_title',)
+	search_fields = ('meta_title', 'partner_description')
+	list_editable = ('is_published',)
+	list_per_page = 25
+
+	def get_categories(self, obj):
+		return ', '.join([c.category_name for c in obj.post_category.all()])
+	get_categories.short_description = 'Categories'
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
