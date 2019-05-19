cda48d682fcdae8791b8b1e9067e00c36036bcb2
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-19 09:58:18 -0700

    separated address fields

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from partners.models import Partner
 from datetime import date
+from localflavor.us.models import USStateField
 
 # Create your models here.
 
@@ -9,8 +10,10 @@ class Event(models.Model):
 	is_published = models.BooleanField(default=True, help_text='Check the box to publish event.')
 	event_start = models.DateField(default=date.today)
 	event_end = models.DateField(default=date.today)
-	event_address = models.CharField(max_length=500)
-	event_partners = models.ManyToManyField(Partner)
+	event_street = models.CharField(max_length=200, blank=True)
+	event_city = models.CharField(max_length=100, blank=True)
+	event_state = USStateField(blank=True)
+	event_partners = models.ManyToManyField(Partner, blank=True)
 
 	def __str__(self):
 		return self.event_name
