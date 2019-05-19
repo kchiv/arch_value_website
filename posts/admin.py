b60e36f56d75d2dc8ca6a490c857cb6a452853da
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-19 10:46:29 -0700

    count function for tags and categories

--- posts/admin.py
+++ posts/admin.py
@@ -38,6 +38,41 @@ class PostAdmin(admin.ModelAdmin):
 	get_image.short_description = 'Featured Image'
 	get_title.short_description = 'Title'
 
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('category_name', 'is_published', 'get_image', 'get_posts')
+	list_display_links = ('category_name', 'get_image')
+	list_editable = ('is_published',)
+	list_per_page = 10
+
+	def get_image(self, obj):
+		# show image in object grid
+		if obj.category_image:
+			# must check if object exists otherwise throws an error
+			# mark_safe() used to display html
+			return mark_safe('<img src="{thumb}" width="100" />'.format(thumb=obj.category_image.url,))
+		else:
+			return 'No image available'
+
+	def get_posts(self, obj):
+		# gets number of posts in category
+		return Post.objects.filter(post_category__pk=obj.pk).count()
+
+	# user friendly names in list display
+	get_image.short_description = 'Featured Image'
+	get_posts.short_description = '# of posts'
+
+class TagAdmin(admin.ModelAdmin):
+	list_display = ('tag_name', 'get_posts')
+	list_display_links = ('tag_name',)
+	list_per_page = 25
+
+	def get_posts(self, obj):
+		# gets number of posts in category
+		return Post.objects.filter(post_tag__pk=obj.pk).count()
+
+	# user friendly names in list display
+	get_posts.short_description = '# of posts'
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
-admin.site.register(Tag)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
