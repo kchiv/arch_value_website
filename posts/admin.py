9abaea3e9bbc67fa039255314b67dbda813d2770
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-19 09:59:16 -0700

    added image thumbnail for list view

--- posts/admin.py
+++ posts/admin.py
@@ -5,25 +5,38 @@ from .models import Post, Category, Tag
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ('meta_title', 'author', 'publication_date', 'is_published', 'get_categories', 'get_image')
-	list_display_links = ('meta_title',)
-	search_fields = ('meta_title', 'partner_description')
+	list_display = ('get_title', 'author', 'publication_date', 'is_published', 'get_categories', 'get_image')
+	list_display_links = ('get_title',)
+	search_fields = ('get_title', 'partner_description')
 	list_editable = ('is_published',)
 	list_per_page = 25
 
 	def get_categories(self, obj):
+		# loop through and dsiplay category names
 		return ', '.join([c.category_name for c in obj.post_category.all()])
-	get_categories.short_description = 'Categories'
 
 	def get_image(self, obj):
 		# show image in object grid
 		if obj.featured_image:
 			# must check if object exists otherwise throws an error
+			# mark_safe() used to display html
 			return mark_safe('<img src="{thumb}" width="100" />'.format(thumb=obj.featured_image.url,))
 		else:
 			return 'No image available'
+
+	def get_title(self, obj):
+		# method foundation for user friendly title name in list display
+		if obj.meta_title:
+			return obj.meta_title
+		else:
+			return 'No title available'
+
 	get_image.allow_tags = True
+
+	# user friendly names in list display
+	get_categories.short_description = 'Categories'
 	get_image.short_description = 'Featured Image'
+	get_title.short_description = 'Title'
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
