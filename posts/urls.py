6ef63d48b804964d8806e689b0465009d0eed9e5
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-21 15:58:04 -0700

    dynamic url for production post view

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('preview/<int:post_id>', views.post_preview, name='post_preview')
+	path('preview/<int:post_id>', views.post_preview, name='post_preview'),
+	path('<slug:post_slug>/<int:post_id>', views.post_detail, name='post_detail'),
 ]
