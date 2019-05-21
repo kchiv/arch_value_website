0c461e56b5602aca74a75239dc25aa3e7aec5581
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-21 11:26:49 -0700

    created post preview

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+
+from .models import Post
 
 # Create your views here.
+
+@staff_member_required
+def post_preview(request, post_id):
+	post = get_object_or_404(Post, pk=post_id)
+
+	context = {
+		'post': post
+	}
+	return render(request, 'posts/post_preview.html', context)
