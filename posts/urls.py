5e06afa4fd1f20770eb26b4c4d6c3b76e414f089
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-22 15:58:32 -0700

    added routing for api endpoint

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path('preview/<int:post_id>', views.post_preview, name='post_preview'),
 	path('<slug:post_slug>/<int:post_id>', views.post_detail, name='post_detail'),
+	path('api/list-post', views.ListPostView.as_view(), name='list_post')
 ]
