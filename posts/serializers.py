165f08f9a2af881ceaf33c978e535e451a791d6e
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-22 15:59:03 -0700

    post model serializer

---
+++ posts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Post
+		fields = ('author',
+				'is_published',
+				'meta_title',
+				'header_title',
+				'meta_description',
+				'publication_date',
+				'modification_date',
+				'featured_image',
+				'thumbnail_image',
+				'post_content',
+				'post_category',
+				'post_tag',
+				'post_slug')
