f91853e8eb23d918b1ab473ed268f5c6f9bad9ae
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-22 15:58:44 -0700

    added list api view

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
-
 from .models import Post
+from rest_framework import generics
+from .serializers import PostSerializer
 
 # Create your views here.
 
@@ -21,3 +22,7 @@ def post_detail(request, post_slug, post_id):
 		'post': post
 	}
 	return render(request, 'posts/post_page.html', context)
+
+class ListPostView(generics.ListAPIView):
+	queryset = Post.objects.all().order_by('-publication_date')
+	serializer_class = PostSerializer
