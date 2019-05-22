c1bfc72d099cfc532ad6515303f3a5855fd3a925
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-22 16:59:40 -0700

    api list view

--- partners/views.py
+++ partners/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Partner
+from rest_framework import generics
+from .serializers import PartnerSerializer
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'events/event_page.html')
+
+class ListPartnerView(generics.ListAPIView):
+	queryset = Partner.objects.all()
+	serializer_class = PartnerSerializer
