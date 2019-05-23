ad11bfc9787cf2b5fd2bd10ee6542712628406dc
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-23 09:23:25 -0700

    routing for api list view

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	path('api/list-event', views.ListEventView.as_view(), name='list_event')
 ]
