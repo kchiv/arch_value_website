ae4239bcf48824b4d8fbb844b6ceafe6eefb042a
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-23 09:23:52 -0700

    api list view

--- events/views.py
+++ events/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Event
+from rest_framework import generics
+from .serializers import EventSerializer
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'events/event_page.html')
+
+class ListEventView(generics.ListAPIView):
+	queryset = Event.objects.all()
+	serializer_class = EventSerializer
