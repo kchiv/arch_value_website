7cba7413fd6a4b10330fc86d377ae191e428d955
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-24 12:00:53 -0700

    added view on site link

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import reverse
 from .models import Page
 
 # Register your models here.
@@ -8,5 +9,10 @@ class PageAdmin(admin.ModelAdmin):
 	list_display_links = ('meta_title',)
 	list_editable = ('is_published', 'show_cta')
 	list_per_page = 25
+
+	def view_on_site(self, obj):
+		# adds link to view from django admin object
+		url = reverse('page_preview', kwargs={'page_id': obj.pk})
+		return url
 
 admin.site.register(Page, PageAdmin)
