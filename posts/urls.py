61f18920d578a5db34e911acdd31a386532e6faf
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-25 14:45:35 -0700

    template based view for blog homepage

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('', views.BlogHomePageView.as_view(), name='index'),
 	path('preview/<int:post_id>', views.post_preview, name='post_preview'),
 	path('<slug:post_slug>/<int:post_id>', views.post_detail, name='post_detail'),
 	path('api/list-post', views.ListPostView.as_view(), name='list_post')
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic.base import TemplateView
 from .models import Post
 from rest_framework import generics
 from .serializers import PostSerializer
 
 # Create your views here.
+
+class BlogHomePageView(TemplateView):
+
+	template_name = 'posts/index.html'
 
 @staff_member_required
 def post_preview(request, post_id):
