2a1551a687adbcc854bcce8cd8b61aeb150e680d
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-25 17:43:14 -0700

    updated url structures

--- partners/urls.py
+++ partners/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('api/list-partner', views.ListPartnerView.as_view(), name='list_partner')
+	path('api/list-partner/', views.ListPartnerView.as_view(), name='list_partner')
 ]
