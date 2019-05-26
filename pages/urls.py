7780a733537306c63dad97703ae2ae6ad1fcad8b
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-25 17:41:39 -0700

    changed url structure to trailing slash

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pages'
+
 urlpatterns = [
-	path('preview/<int:page_id>', views.page_preview, name='page_preview'),
-	path('<slug:page_slug>', views.page_detail, name='page_detail')
+	path('preview/<slug:page_slug>/', views.page_preview, name='page_preview'),
+	path('<slug:page_slug>/', views.page_detail, name='page_detail')
 ]
