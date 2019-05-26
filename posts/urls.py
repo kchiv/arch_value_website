08b63a65af9e8bf8d13766a8c5f2cf0292fb4fe0
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-25 17:43:50 -0700

    updated url structures

--- posts/urls.py
+++ posts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
 	path('', views.BlogHomePageView.as_view(), name='index'),
-	path('preview/<int:post_id>', views.post_preview, name='post_preview'),
-	path('<slug:post_slug>/<int:post_id>', views.post_detail, name='post_detail'),
-	path('api/list-post', views.ListPostView.as_view(), name='list_post')
+	path('preview/<slug:post_slug>/<int:post_id>/', views.post_preview, name='post_preview'),
+	path('<slug:post_slug>/<int:post_id>/', views.post_detail, name='post_detail'),
+	path('api/list-post/', views.ListPostView.as_view(), name='list_post')
 ]
