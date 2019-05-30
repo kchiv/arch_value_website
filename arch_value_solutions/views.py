c39f0e712be3814ab2b0896d37f915cc898afc83
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-30 13:17:36 -0700

    passed post object to homepage view

--- arch_value_solutions/views.py
+++ arch_value_solutions/views.py
@@ -1,8 +1,14 @@
 from django.views.generic.base import TemplateView
+from posts.models import Post
 
 class HomePageView(TemplateView):
 
 	template_name = 'template_view/index.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['posts'] = Post.objects.filter(is_published=True).order_by('-publication_date')[:3]
+		return context
 
 
 class ContactPageView(TemplateView):
