5cec94265c7bedb5eb57411fa6560e306eedd714
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-31 16:50:11 -0700

    developed contacts model

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Contact(models.Model):
+	user_type = models.CharField(max_length=20, blank=True)
+	name = models.CharField(max_length=50, blank=True)
+	email = models.EmailField(blank=True)
+	phone_number = models.CharField(max_length=50, blank=True)
+	company_name = models.CharField(max_length=100, blank=True)
+	message = models.CharField(max_length=1000, blank=True)
+	subscriber = models.BooleanField(default=False)
+	contact_date = models.DateTimeField(default=datetime.now, blank=True)
+
+	def __str__(self):
+		return self.name
