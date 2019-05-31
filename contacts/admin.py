6ecb140f345628b6617de0ffd1656d963a094753
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-05-31 16:49:55 -0700

    added admin registration

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Contact
+
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('name', 'user_type', 'email', 'company_name', 'contact_date')
+	list_display_links = ('name', 'email')
+	search_fields = ('name', 'email', 'company_name')
+	list_per_page = 25
+
+admin.site.register(Contact, ContactAdmin)
