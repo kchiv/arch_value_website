caf4f3b12b091d9c9b386a5f8c63da05b9d11ad4
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-01 00:15:14 -0700

    removed commented code

--- contacts/views.py
+++ contacts/views.py
@@ -11,29 +11,12 @@ def hp_contact_form(request):
 		phone_number = request.POST['user_phone']
 		company_name = request.POST['user_company']
 		message = request.POST['user_message']
+		
 		if 'user_subscribe' in request.POST:
 			subscriber = True
 		else:
 			subscriber = False
 
-		# error_template = 'The {} entered must be less than {} characters.'
-
-		# if len(name) > 50:
-		# 	error_message = error_template.format('name', '50')
-		# 	return redirect('home')
-		# else:
-		# 	if len(email) > 254:
-		# 		error_message = error_template.format('email', '254')
-		# 	else:
-		# 		if len(phone_number) > 50:
-		# 			error_message = error_template.format('phone number', '50')
-		# 		else:
-		# 			if len(company_name) > 100:
-		# 				error_message = error_template.format('company name', '100')
-		# 			else:
-		# 				if len(message) > 1000:
-		# 					error_message = error_template.format('message', '1,000')
-		# 				else:
 		contact = Contact(user_type = user_type,
 							name = name,
 							email = email,
