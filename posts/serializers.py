0194003abcb06b2c4d7698e89c9f62c8497a26f4
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-03 18:06:14 -0700

    added method fields for short content

--- posts/serializers.py
+++ posts/serializers.py
@@ -13,7 +13,9 @@ class UserSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
 	author = UserSerializer()
 	post_title = serializers.SerializerMethodField()
+	shorter_title = serializers.SerializerMethodField()
 	strip_content = serializers.SerializerMethodField()
+	shorter_content = serializers.SerializerMethodField()
 	thumb_img = serializers.SerializerMethodField()
 	feat_img = serializers.SerializerMethodField()
 	post_url = serializers.SerializerMethodField()
@@ -25,10 +27,21 @@ class PostSerializer(serializers.ModelSerializer):
 		else:
 			return obj.header_title
 
+	def get_shorter_title(self, obj):
+		if len(obj.header_title) > 60:
+			return obj.header_title[:60] + '...'
+		else:
+			return obj.header_title
+
 	def get_strip_content(self, obj):
 		remove_tags = strip_tags(obj.post_content)
 		strip_newline = sub(r'\r\n', ' ', remove_tags)
 		return strip_newline[:150] + '...'
+
+	def get_shorter_content(self, obj):
+		remove_tags = strip_tags(obj.post_content)
+		strip_newline = sub(r'\r\n', ' ', remove_tags)
+		return strip_newline[:90] + '...'
 
 	def get_thumb_img(self, obj):
 		return obj.thumbnail_image.url
@@ -44,5 +57,5 @@ class PostSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Post
-		fields = ('id', 'author', 'is_published', 'pub_date', 'post_title', 'strip_content', 'post_category', 'thumb_img', 'feat_img', 'post_url')
+		fields = ('id', 'author', 'is_published', 'pub_date', 'post_title', 'shorter_title', 'strip_content', 'shorter_content', 'post_category', 'thumb_img', 'feat_img', 'post_url')
 		depth = 1
