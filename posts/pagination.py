809f3cbc30629bd7a1ad9d2cbdd21f0bce623a88
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-04 17:19:43 -0700

    added new pagination class for customized fields in api

--- posts/pagination.py
+++ posts/pagination.py
@@ -1,6 +1,26 @@
+from django.conf import settings
 from rest_framework import pagination
 
+
+class YourPagination(pagination.PageNumberPagination):
+	page_size = 3
+	page_size_query_param = 'page_size'
+	max_page_size = 1000
+
+	def get_paginated_response(self, data):
+		response = super(YourPagination, self).get_paginated_response(data)
+		response.data['total_pages'] = self.page.paginator.num_pages
+		if self.page.has_previous():
+			response.data['prev_page_num'] = self.page.previous_page_number()
+		else:
+			response.data['prev_page_num'] = 0
+		if self.page.has_next():
+			response.data['nex_page_num'] = self.page.next_page_number()
+		else:
+			response.data['nex_page_num'] = 0
+		return response
+
 class StandardResultsPagination(pagination.PageNumberPagination):
-	page_size = 10
+	page_size = 1
 	page_size_query_param = 'page_size'
 	max_page_size = 1000
