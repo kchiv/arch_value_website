5348c027280680c9e452e48eb1f32d99684c0330
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-06 14:47:27 -0700

    updated category api url string

--- posts/urls.py
+++ posts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
 	path('preview/<slug:post_slug>/<int:post_id>/', views.post_preview, name='post_preview'),
 	path('<slug:post_slug>/<int:post_id>/', views.post_detail, name='post_detail'),
 	path('api/list-post/', views.ListPostView.as_view(), name='list_post'),
-	path('api/<slug:category_slug>/', views.CategoryPostListView.as_view(), name='category_list_post')
+	path('api/category/<slug:category_slug>/', views.CategoryPostListView.as_view(), name='category_list_post')
 ]
