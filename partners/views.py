135e4f10fa015571f830be767dac7d54ac10c11f
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 15:46:56 -0700

    added list and detail api views

--- partners/views.py
+++ partners/views.py
@@ -1,6 +1,6 @@
 from .models import Partner
 from rest_framework import generics
-from .serializers import ListPartnerSerializer
+from .serializers import ListPartnerSerializer, DetailPartnerSerializer
 from .pagination import PartnersResultsPagination
 
 # Create your views here.
@@ -9,3 +9,8 @@ class ListPartnerView(generics.ListAPIView):
 	queryset = Partner.objects.filter(is_published=True).order_by('partner_listing_order')
 	serializer_class = ListPartnerSerializer
 	pagination_class = PartnersResultsPagination
+
+class DetailPartnerView(generics.RetrieveAPIView):
+	lookup_field = 'id'
+	queryset = Partner.objects.all()
+	serializer_class = DetailPartnerSerializer
