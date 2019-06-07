9448f5d7b010e5f50069864e6ac989889ab6efb2
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 13:54:55 -0700

    developed pagination for events api

---
+++ events/pagination.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from rest_framework import pagination
+
+
+class EventsResultsPagination(pagination.PageNumberPagination):
+	page_size = 6
+	page_size_query_param = 'page_size'
+	max_page_size = 1000
+
+	def get_paginated_response(self, data):
+		response = super(EventsResultsPagination, self).get_paginated_response(data)
+		response.data['total_pages'] = self.page.paginator.num_pages
+		if self.page.has_previous():
+			response.data['prev_page_num'] = self.page.previous_page_number()
+		else:
+			response.data['prev_page_num'] = None
+
+		if self.page.has_next():
+			response.data['nex_page_num'] = self.page.next_page_number()
+		else:
+			response.data['nex_page_num'] = None
+		return response
