e0edf05787d17dbf23f1e5559972bd097ebfc229
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 13:53:55 -0700

    added api url paths for list and detail views

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('api/list-event', views.ListEventView.as_view(), name='list_event')
+	path('api/list-event/', views.ListEventView.as_view(), name='list_event'),
+	path('api/event/<int:id>/', views.DetailEventView.as_view(), name='detail_event')
 ]
