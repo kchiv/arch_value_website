165b1c9b5dac37457307fb78dfab190fb87c62eb
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 15:04:47 -0700

    list and detail serializers started

--- partners/serializers.py
+++ partners/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Partner
 
-class PartnerSerializer(serializers.ModelSerializer):
+class ListPartnerSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Partner
+		fields = '__all__'
+		depth = 1
+
+class DetailPartnerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Partner
 		fields = '__all__'
