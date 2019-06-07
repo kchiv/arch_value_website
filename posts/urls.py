1d6b50e8459a956c0061a5357dbfe48e9c00c622
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 15:07:52 -0700

    added url routing for tag api view

--- posts/urls.py
+++ posts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
 	path('preview/<slug:post_slug>/<int:post_id>/', views.post_preview, name='post_preview'),
 	path('<slug:post_slug>/<int:post_id>/', views.post_detail, name='post_detail'),
 	path('api/list-post/', views.ListPostView.as_view(), name='list_post'),
-	path('api/category/<slug:category_slug>/', views.CategoryPostListView.as_view(), name='category_list_post')
+	path('api/category/<slug:category_slug>/', views.CategoryPostListView.as_view(), name='category_list_post'),
+	path('api/tag/<int:tag_id>/', views.TagListPostView.as_view(), name='tag_list_post')
 ]
