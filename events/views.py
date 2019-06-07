1d3e00204276e82bf8c214551032f1e9b1d1148e
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 13:54:33 -0700

    created list and detail views

--- events/views.py
+++ events/views.py
@@ -1,9 +1,18 @@
+from datetime import datetime
 from .models import Event
 from rest_framework import generics
-from .serializers import EventSerializer
+from .serializers import DetailEventSerializer, ListEventSerializer
+from .pagination import EventsResultsPagination
 
 # Create your views here.
 
 class ListEventView(generics.ListAPIView):
+	current_date = datetime.now().date()
+	queryset = Event.objects.filter(is_published=True, event_end__gte=current_date).order_by('-event_end')
+	serializer_class = ListEventSerializer
+	pagination_class = EventsResultsPagination
+
+class DetailEventView(generics.RetrieveAPIView):
+	lookup_field = 'id'
 	queryset = Event.objects.all()
-	serializer_class = EventSerializer
+	serializer_class = DetailEventSerializer
