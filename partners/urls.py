966a0694783995c1c917c6f87223bbe709a41805
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 15:46:44 -0700

    url routing for apis

--- partners/urls.py
+++ partners/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('api/list-partner/', views.ListPartnerView.as_view(), name='list_partner')
+	path('api/list-partner/', views.ListPartnerView.as_view(), name='list_partner'),
+	path('api/partner/<int:id>', views.DetailPartnerView.as_view(), name='detail_partner')
 ]
