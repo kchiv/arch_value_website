676a11124024af3ecff22ded625f519160c1fd96
Kyle Chivers <kchiv@kyles-mbp.lan>
2019-06-07 13:52:01 -0700

    made published field editable from list view

--- events/admin.py
+++ events/admin.py
@@ -4,9 +4,10 @@ from .models import Event
 # Register your models here.
 
 class EventAdmin(admin.ModelAdmin):
-	list_display = ('event_name', 'event_start', 'event_end', 'event_country', 'event_city', 'event_state', 'get_partners')
+	list_display = ('event_name', 'is_published', 'event_start', 'event_end', 'event_country', 'event_city', 'event_state', 'get_partners')
 	list_display_links = ('event_name',)
 	search_fields = ('event_name', 'event_country', 'event_city', 'event_state', 'event_partners')
+	list_editable = ('is_published',)
 	list_per_page = 25
 
 	def get_partners(self, obj):
