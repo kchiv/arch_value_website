2440fb191b4a3c68265039c4dafd300feeb09941
Kyle Chivers <chiverskyle@gmail.com>
2019-06-27 08:22:47 -0700

    increased object counts for pagination

--- events/pagination.py
+++ events/pagination.py
@@ -3,7 +3,7 @@ from rest_framework import pagination
 
 
 class EventsResultsPagination(pagination.PageNumberPagination):
-	page_size = 6
+	page_size = 8
 	page_size_query_param = 'page_size'
 	max_page_size = 1000
 
