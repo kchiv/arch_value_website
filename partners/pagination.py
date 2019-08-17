b8ac84c5d264a01665a6e3514cb48b34fb7bb667
Kyle Chivers <chiverskyle@gmail.com>
2019-08-17 12:55:13 -0700

    increased pagination to 12 objects

--- partners/pagination.py
+++ partners/pagination.py
@@ -3,7 +3,7 @@ from rest_framework import pagination
 
 
 class PartnersResultsPagination(pagination.PageNumberPagination):
-	page_size = 9
+	page_size = 12
 	page_size_query_param = 'page_size'
 	max_page_size = 1000
 
